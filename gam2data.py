16ba7ec053945ae878a5ff30da017ffd935a8e76
Isaac Wetton <isaac@wetton.net>
2023-11-30 23:35:11 +0000

    Data generation and plotting

--- gam2data.py
+++ gam2data.py
@@ -11,7 +11,7 @@ initial_Jdist = 1000 * constants.R_JUPITER
 
 # Define delta & phis
 delta = 0.778
-phis = np.linspace(0.0833 - 0.01, 0.0833 + 0.01, 50)
+phis = np.linspace(0.0833 - 0.01, 0.0833 + 0.01, 100)
 
 # Define lists to store results
 bests = []
@@ -40,7 +40,7 @@ for phi in phis:
     moons = [bodies.get_io(), bodies.get_europa(), bodies.get_ganymede(), bodies.get_callisto()]
 
     # Find initial moon states
-    moon_states = []
+    moon_states = [None] * 4
     for i in range(4):
         moon_states[i] = moons[i](T)
 
@@ -57,11 +57,11 @@ for phi in phis:
 
     # Evolve and record least negative semi-major axis
     best = util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity), constants.MU_JUPITER)
-    while Jdist <= initial_Jdist:
+    while Jdist <= initial_Jdist and best < 0:
         # Calculate acceleration
         orbiter.acceleration = orbiter.updateGravitationalAcceleration(jupiter)
         for i in range(4):
-            orbiter.acceleration = orbiter.updateGravitationalAcceleration(moon_obj[i])
+            orbiter.acceleration += orbiter.updateGravitationalAcceleration(moon_obj[i])
 
         # Evolve with timestep depending on Jupiter distance
         if Jdist > 50 * constants.R_JUPITER:
@@ -77,11 +77,37 @@ for phi in phis:
         if best < util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity), constants.MU_JUPITER):
             best = util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity), constants.MU_JUPITER)
 
+    # Continue evolution if capture is accomplished
+    if best > 0:
+        print("capture")
+        for i in range(100000):
+            # Calculate acceleration
+            orbiter.acceleration = orbiter.updateGravitationalAcceleration(jupiter)
+            for i in range(4):
+                orbiter.acceleration += orbiter.updateGravitationalAcceleration(moon_obj[i])
+
+            # Evolve with timestep depending on Jupiter distance
+            if Jdist > 50 * constants.R_JUPITER:
+                deltaT = 200
+            else:
+                deltaT = 2
+
+            T += deltaT
+            orbiter.update_eulerCromer(deltaT)
+            Jdist = np.linalg.norm(orbiter.position - jupiter.position)
+
+            # Determine whether semi-major axis is better
+            if best < util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity),
+                                     constants.MU_JUPITER):
+                best = util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity),
+                                      constants.MU_JUPITER)
+
     # Append best semi-major axis to semi-majors
     bests.append(best)
     finals.append(util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity), constants.MU_JUPITER))
+    print(best)
 
 # Save data
-f = open("data/gam2data.dat", "wb")
+f = open("data/gam2data3.dat", "wb")
 pickle.dump((phis, bests, finals), f, True)
 f.close()
---
+++ gam2plot.py
@@ -0,0 +1,14 @@
+import pickle
+from matplotlib import pyplot as plt
+
+# Load data
+f = open("data/gam2data3.dat", "rb")
+phis, bests, finals = pickle.load(f)
+f.close()
+
+# Create plot
+plt.plot(phis, bests)
+plt.title(r"A plot of best semi-major axis of Jupiter orbit against $\phi$")
+plt.xlabel(r"$\phi$ (rad)")
+plt.ylabel("Semi-major axis")
+plt.show()
