126bf0d9e7aae6b67a46632bc314e39c959fe225
Isaac Wetton <isaac@wetton.net>
2023-12-18 14:51:24 +0000

    Backwards evolution

--- gam4.py
+++ gam4.py
@@ -13,9 +13,6 @@ initial_Jdist = 1000 * constants.R_JUPITER
 delta = 0.778
 phi = 0.0831
 
-# Define tuple to store closest approach time & distance
-closest = (constants.EPOCH, 1000 * constants.R_JUPITER)
-
 # Initialise Jupiter
 jupiter = Particle.Particle(name="Jupiter", mu=constants.MU_JUPITER)
 
@@ -28,6 +25,9 @@ initvel = np.array([3.4 * np.cos(vel_angle), 3.4 * np.sin(vel_angle), 0], dtype=
 
 # Initialise orbiter
 orbiter = Particle.Particle(position=initpos, velocity=initvel, name="Orbiter", mu=2000.0 * G)
+
+# Define tuple to store closest approach time & distance
+closest = (constants.EPOCH, 1000 * constants.R_JUPITER, orbiter)
 
 # Initial time
 T = constants.EPOCH
@@ -75,6 +75,45 @@ while Jdist <= initial_Jdist:
 
     # Determine whether closest approach is better
     if np.linalg.norm(orbiter.position - moon_obj[3].position) < closest[1]:
-        closest = (T, np.linalg.norm(orbiter.position - moon_obj[3].position))
+        closest = (T, np.linalg.norm(orbiter.position - moon_obj[3].position), orbiter)
 
+# Print closest approach time, distance and orbiter object
 print(closest)
+
+# Move the orbiter towards Callisto, to an altitude of 50km, and evolve backwards
+orbiter = closest[2]
+T = closest[0]
+for i in range(4):
+    moon_states[i] = moons[i](T)
+    moon_obj[i].position = np.array(moon_states[i][0:3], dtype=float)
+
+orb_to_cal_unitvec = (moon_obj[3].position - orbiter.position) / np.linalg.norm(moon_obj[3].position - orbiter.position)
+orbiter.position += orb_to_cal_unitvec * (closest[1] - constants.R_CALLISTO - 50)
+Jdist = np.linalg.norm(orbiter.position - jupiter.position)
+
+while Jdist <= initial_Jdist:
+    # Calculate acceleration
+    orbiter.acceleration = orbiter.updateGravitationalAcceleration(jupiter)
+    for i in range(4):
+        orbiter.acceleration += orbiter.updateGravitationalAcceleration(moon_obj[i])
+
+    # Evolve with timestep depending on Jupiter distance
+    if Jdist > 50 * constants.R_JUPITER:
+        deltaT = -200
+    else:
+        deltaT = -2
+
+    T += deltaT
+    orbiter.update_eulerCromer(deltaT)
+    Jdist = np.linalg.norm(orbiter.position - jupiter.position)
+
+    # Update moon parameters
+    for i in range(4):
+        moon_states[i] = moons[i](T)
+        moon_obj[i].position = np.array(moon_states[i][0:3], dtype=float)
+
+# Print results
+print("Orbiter initial position vector: ", orbiter.position)
+print("Orbiter initial velocity vector: ", orbiter.velocity)
+print("Orbiter initial velocity: ", np.linalg.norm(orbiter.velocity))
+print("Initial time: ", T)
