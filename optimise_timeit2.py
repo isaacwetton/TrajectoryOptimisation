96cfc6970ecbae3425a44bc3b1018296f60e45f6
Isaac Wetton <isaac@wetton.net>
2024-01-30 15:08:07 +0000

    Monte quadratic test is now single-stage

--- optimise_timeit2.py
+++ optimise_timeit2.py
@@ -16,7 +16,7 @@ def quadratic(x):
 """
 
 # simann_decs = ["0.1", "0.05", "0.01", "0.005", "0.001", "0.0005", "0.0001", "0.00005", "0.00001", "0.000005"]
-simann_decs = np.exp(np.linspace(-12, 0, 100))
+simann_decs = np.exp(np.linspace(-12, -3, 100))
 simann_times = []
 simann_errors = []
 
@@ -31,17 +31,17 @@ for i in range(len(simann_decs)):
     simann_errors.append(errors / tests)
 
 # monte_sols = ["10", "50", "100", "1000", "5000", "10000", "50000", "100000"]
-monte_sols = np.rint(np.exp(np.linspace(2, 15, 100)))
+monte_sols = np.rint(np.exp(np.linspace(3, 15, 100)))
 monte_times = []
 monte_errors = []
 
 for i in range(len(monte_sols)):
-    t_monte = timeit("result, vals = optimise.montecarlo(quadratic, 0, " + str(int(monte_sols[i])) + ", 10, (-5.0, 5.0))",
+    t_monte = timeit("result, vals = optimise.montecarlo(quadratic, 0, " + str(int(monte_sols[i])) + ", 0, (-5.0, 5.0))",
                      setup=setup, number=tests)
     monte_times.append(t_monte / tests)
     errors = 0
     for j in range(tests):
-        result, vals = optimise.montecarlo(quadratic, 0, int(monte_sols[i]), 1, (-5.0, 5.0))
+        result, vals = optimise.montecarlo(quadratic, 0, int(monte_sols[i]), 0, (-5.0, 5.0))
         errors += abs(result)
     monte_errors.append(errors / tests)
 
