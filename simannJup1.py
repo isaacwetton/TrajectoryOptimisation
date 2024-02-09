117d4cb5cd7d936c2c84f39e36b856ade9b650ef
Isaac Wetton <isaac@wetton.net>
2024-02-09 17:04:00 +0000

    Fixed bug where process doesn't terminate correctly

--- simannJup1.py
+++ simannJup1.py
@@ -94,7 +94,7 @@ def find_semimajor(delta, phi, MJD):
 
         # Update closest Jupiter approach and break if leaving 30 Jupiter radii
         if closest_jup > Jdist:
-            Jdist = closest_jup
+            closest_jup = Jdist
         elif Jdist > closest_jup + 10 * constants.R_JUPITER:
             break
         elif 10 * constants.R_JUPITER > util.semimajor(np.linalg.norm(orbiter.position), np.linalg.norm(orbiter.velocity), constants.MU_JUPITER) > 0:
